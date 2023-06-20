cca42ffb290b1224adffdeb51fb3c173c1283acc
Davinder Kumar <abhiverma97819@gmail.com>
2023-06-20 12:37:08 +0530

    footer.jsx

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Icons } from '../../components/Icons';
-import profile from '../../Images/profile.jpg'
+import profile from '../../Images/me.png'
 
 const Footer = () => {
   return (
